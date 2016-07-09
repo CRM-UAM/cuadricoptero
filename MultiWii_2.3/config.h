432ddf8c382323f76d11af97027ba6e1f67db41b
Carlos Garcia Saura <carlosgarciasaura@gmail.com>
2016-07-09 18:11:20 +0200

    Moved 2.3 to separate folder

--- MultiWii_2.3/config.h
+++ MultiWii_2.3/config.h
@@ -499,7 +499,7 @@ At this moment you can use this function only with WinGUI 2.3 release. MultiWiiC
 
   /******                Serial com speed    *********************************/
     /* This is the speed of the serial interfaces */
-    #define SERIAL0_COM_SPEED 115200
+    #define SERIAL0_COM_SPEED 9600
     #define SERIAL1_COM_SPEED 115200
     #define SERIAL2_COM_SPEED 115200
     #define SERIAL3_COM_SPEED 115200
